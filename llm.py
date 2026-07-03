3e1debe16b2e3d8c4e11103a82566e1e59110728
Sambuddha-Pal <Sambuddhapal123@gmail.com>
2026-07-03 12:42:32 +0530

    cleaned repo

---
+++ llm.py
@@ -0,0 +1,14 @@
+import os
+import httpx
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+client=httpx.Client(verify=False)
+llm = ChatOpenAI(
+    base_url="https://genailab.tcs.in/",
+    model="genailab-maas-gpt-35-turbo",
+    api_key="sk-ZalSmsmKK5U9yBHG08zVHQ",
+    http_client=client
+)
+print(llm.invoke("Hello, how are you?"))
